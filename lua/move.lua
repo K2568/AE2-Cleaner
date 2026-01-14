5823ad2265d13b3942933d51ade72c76f33bcf26
K256 <95189223+K2568@users.noreply.github.com>
2026-01-14 19:33:06 +0000

    Update move.lua

--- lua/move.lua
+++ lua/move.lua
@@ -1,10 +1,13 @@
 me = peripheral.wrap("right")
 write("Which mod to purge> ")
 local choice = read()
-local items = io.open(choice, "r")
+local items = io.open(choice.."txt", "r")
 if items then
     for line in items:lines() do
+        itemId = choice..":"..line
         print(line)
+        num = me.getItem({name=itemId})
+        me.exportItem({name=itemId, count=num}, "up")
     end
-    file:close()
+    items:close()
 end
