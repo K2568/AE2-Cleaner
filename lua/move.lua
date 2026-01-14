fda6c1b32e300a164ccb2c730c36bfdc68b06676
K256 <95189223+K2568@users.noreply.github.com>
2026-01-14 21:57:09 +0000

    Move check to a more appropriate space

--- lua/move.lua
+++ lua/move.lua
@@ -16,14 +16,14 @@ if items then
             if num < 5000 then
                  me.exportItem({name=itemId, count=num}, "up")
                  print("Items moved")
+                 sleep(0.5)
+                 print("Awaiting redstone pulse")
+                os.pullEvent("redstone")
+                print("Redstone fired")
             else
                 print("More than 5k of this item")
         end
-        sleep(0.5)
         print()
-        print("Awaiting redstone pulse")
-        os.pullEvent("redstone")
-        print("Redstone fired")
     end
     items:close()
 end
